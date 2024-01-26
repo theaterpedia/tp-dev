0cbb5b15d59a47925c57adc61d4ab9849d7b1663
Hans <hans.doenitz@dasei.eu>
2024-01-26 15:13:24 +0100

    add monaspace-logo

--- pages/admin.vue
+++ pages/admin.vue
@@ -4,7 +4,16 @@
     <RenderContent :content="component.fields.content" />
   </!-->
   <!-- ContentDoc /-->
-  <h1 class="text-2xl">DEV</h1>
+  <h1 class="text-4xl font-mona">DEV && Admin-Page</h1>
+    <p class="text-xl font-mona">
+      here you find dev-content for testing-purpose and for managing the migration from the vsf-shop to the pruvious-driven setup
+    </p>
+    <p class="text-xl font-mona">
+      premium
+    </p>
+    <p class="text-xl font-mono">
+      premium
+    </p>  
 </template>
 
 <script setup lang="ts">
