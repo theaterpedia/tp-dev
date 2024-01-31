4aec67884756584e64b85bec679837d02323bfed
Hans <hans.doenitz@dasei.eu>
2024-01-31 09:00:56 +0100

    fixed tailwind-config in theme-layer > now tw-based @crearis/vue renders correctly

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,19 @@
   <!-- div v-for="(component, index) in content" :key="`${component.fields.component}-${index}`">
     <RenderContent :content="component.fields.content" />
   </!-->
+  <!-- ContentDoc /-->
+  <NuxtLayout>
+  <h1 class="text-4xl font-mona">DEV && Admin-Page</h1>
+    <p class="text-xl font-mona">
+      here you find dev-content for testing-purpose and for managing the migration from the vsf-shop to the pruvious-driven setup
+    </p>
+    <p class="text-xl font-mona">
+      premium
+    </p>
+    <p class="text-xl font-mono">
+      premium
+    </p>
+    </NuxtLayout>
 </template>
 
 <script setup lang="ts">
